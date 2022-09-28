d5b93da82681d22313e238d4ac59bfc6dba288c1
Gustavo Tavares <gmartinez-de-oliveir@myseneca.ca>
2022-09-28 15:20:14 -0400

    Changing fetch links to Heroku

--- src/pages/Feed/SinglePost/SinglePost.js
+++ src/pages/Feed/SinglePost/SinglePost.js
@@ -33,7 +33,7 @@ class SinglePost extends Component {
         postId: postId,
       },
     };
-    fetch(`http://localhost:8080/graphql`, {
+    fetch(`https://blog-restapi-nodejs.herokuapp.com/graphql`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${this.props.token}`,
@@ -53,7 +53,7 @@ class SinglePost extends Component {
         this.setState({
           title: resData.data.post.title,
           author: resData.data.post.creator.name,
-          image: `http://localhost:8080/${resData.data.post.imageUrl}`,
+          image: `https://blog-restapi-nodejs.herokuapp.com/${resData.data.post.imageUrl}`,
           date: new Date(resData.data.post.createdAt).toLocaleDateString(
             "en-US"
           ),
